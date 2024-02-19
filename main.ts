07072dc39b0141d34d205c6d3605f859a9e301ea
Yoshio HANAWA <y@hnw.jp>
2024-02-19 23:46:16 +0900

    Apply oidc-auth middleware

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 import { Hono } from 'npm:hono'
 import { serveStatic } from 'npm:hono/deno'
+import { oidcAuthMiddleware, getAuth, revokeSession } from 'npm:@hono/oidc-auth';
 
 const app = new Hono()
 
-app.use('/*', serveStatic({ root: 'public/' }))
+app.get('/logout', async (c) => {
+  await revokeSession(c)
+  return c.text('You have been successfully logged out!')
+})
+app.use('*', oidcAuthMiddleware())
+app.use('*', async (c, next) => {
+  // Authorize user with email address
+  const auth = await getAuth(c)
+  if (!auth?.email.endsWith('@gmail.com')) {
+    return c.text('Unauthorized', 401)
+  }
+  await next()
+})
+
+app.use('*', serveStatic({ root: 'public/' }))
 
 Deno.serve(app.fetch)
